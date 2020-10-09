9d6fb4bf662f0c4d49c3274d794a2c0994458ac9
orhun <orhunparmaksiz@gmail.com>
2020-10-09 23:13:56 +0300

    chore: Bump the version variable (1.10)

--- god.go
+++ god.go
@@ -18,7 +18,7 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
-var version = "1.9"
+var version = "1.10"
 var outb, errb bytes.Buffer                 // Buffer for command output
 var cmdSlice, gitCmdSlice, allCmds []string // Slice for the shortened git commands
 var gitShortcuts [][]string                 // 2-d slice for the git shortcuts
